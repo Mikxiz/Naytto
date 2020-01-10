72e26e599f6413cbd213ea61e39fa32992f8019c
Mikxiz <mika.kilpia@gmail.com>
2020-01-10 13:32:58 +0200

    Modelit tehty

---
+++ Naytto/Naytto/Controllers/ResponsiveSiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Naytto.Controllers
+{
+    public class ResponsiveSiteController : Controller
+    {
+        // GET: ResponsiveSite
+        public ActionResult RwdSite()
+        {
+            return View();
+        }
+    }
+}
